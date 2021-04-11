d83274189adcbb260979e90898c93c794017b2d8
Chiraag Anand <67359421+Snail664@users.noreply.github.com>
2021-04-11 10:44:47 +0800

    Updates

---
+++ precache-manifest.3939628d0ca284df72c90cc7611331ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4500608b825e2d6ae24e90e0090dc19a",
+    "url": "/amazonClone/index.html"
+  },
+  {
+    "revision": "5019f13f9c46b97173c4",
+    "url": "/amazonClone/static/css/main.b628fe9e.chunk.css"
+  },
+  {
+    "revision": "4443ac74e136dc126859",
+    "url": "/amazonClone/static/js/2.bf194317.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazonClone/static/js/2.bf194317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5019f13f9c46b97173c4",
+    "url": "/amazonClone/static/js/main.54c3eef3.chunk.js"
+  },
+  {
+    "revision": "07bb2257b49cd02c796e",
+    "url": "/amazonClone/static/js/runtime-main.18de613d.js"
+  }
+]);
